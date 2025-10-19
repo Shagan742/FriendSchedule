4b94d893219b5fb2a41f5333bedf163842e4bdee
Shagan742 <526ssangha@frhsd.com>
2025-10-19 23:47:11 +0000

    code finally works, need to make amelia json and style more

--- script.js
+++ script.js
@@ -3,29 +3,38 @@ let output = document.getElementById("output")
 
 async function loadSchedule(file) {
 output.innerHTML=''
+
+
+//this edits the the file string to let me put an h3 where I can identify whose schedule I'm showing
+let personName=file.slice(1, file.length-5);
+let capitalizedInitial=file.charAt(0).toUpperCase();
+let completedName=capitalizedInitial+personName;
     try {
         const res = await fetch(file);
         const data = await res.json();
-
-        data.forEach(class => {
+output.insertAdjacentHTML('beforeend', `<h3 class="name mt-5 mb-3">${completedName}'s schedule</h3>`)
+        
+        for(const classes of data) {
             output.insertAdjacentHTML('beforeend',`
+
+                <div class="col-4 m-3">
             <div class="card h-100">
-  <div class="card-img" id="rectangle">
   <div class="card-body">
     <p class="card-text">
+    <strong class="gottaEdit">Period ${classes.period}</strong>
     <ul>
-        <li>${class.className}</li>
-        <li>${class.teacher}</li>
-        <li>${class.roomNumber}</li>
-        <li>${class.period}</li>
-        <li>${class.subjectArea}</li>
+        <li><strong>Class Name:</strong> ${classes.className}</li>
+        <li><strong>Teacher:</strong> ${classes.teacher}</li>
+        <li><strong>Room</strong> ${classes.roomNumber}</li>
+        <li><strong>Subject Area:</strong> ${classes.subjectArea}</li>
     </ul>
     </p>
   </div>
+</div>
 </div>`)
 
 
-        })
+        }
     } catch (err) {
         output.insertAdjacentHTML('beforeend', 'Error: ' + err)
     }
@@ -34,15 +43,19 @@ output.innerHTML=''
 
 }
 
-
-document.getElementById("output").addEventListener("keydown", btn => {
+loadSchedule("shagan.json")
+document.getElementById("btn").addEventListener("keydown", btn => {
     if (btn.key === '1') {
-        loadSchedule("shagan.json")
-    } else if (btn.key === '2') {
         loadSchedule("kristen.json")
-    } else if (btn.key === '3') {
+    } else if (btn.key === '2') {
         loadSchedule("yasmina.json")
-    } else if (btn.key === '4') {
-        loadSchedule("shagan.json")
+    } else if (btn.key === '3') {
+        loadSchedule("amelia.json")
+    } else {
+       loadSchedule("shagan.json")
     }
+
+    
+
 })
+
