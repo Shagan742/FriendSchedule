fc30af576a8b44e7ba627790a932b5e646925068
Shagan742 <526ssangha@frhsd.com>
2025-10-21 23:22:35 +0000

    finished

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let output = document.getElementById("output")
+let output = document.getElementById("output") //define output
 
 
-async function loadSchedule(file) {
-    output.innerHTML = ''
+async function loadSchedule(file) { //function loads and that file is the file i want to load under eventListener conditions below
+    output.innerHTML = '' //empties HTML before it
 
 
-    //this edits the the file string to let me put an h3 where I can identify whose schedule I'm showing
+    //this edits the the file thing to let me put an h3 where I can identify whose schedule I'm showing
     let personName = file.slice(1, file.length - 5);
     let capitalizedInitial = file.charAt(0).toUpperCase();
     let completedName = capitalizedInitial + personName;
@@ -15,7 +15,7 @@ async function loadSchedule(file) {
 
     //try to get the json file and do stuff with it
     try {
-        const res = await fetch(file);
+        const res = await fetch(file); //get the file and convert it to what i need
         const data = await res.json();
 
         //whoever's schedule is shown is identified
@@ -23,6 +23,7 @@ async function loadSchedule(file) {
         
 //for every class of my data i got, put the person's info abt each class in cards
         for (const classes of data) {
+            //decided to insert HTML the long way, making it = some big card string with json data in it
             const html = `
 
                 <div class="col-md-4 col-sm-10 my-3">
@@ -40,11 +41,10 @@ async function loadSchedule(file) {
   </div>
 </div>
 </div>`
-            output.insertAdjacentHTML('beforeend', html)
-
+            output.insertAdjacentHTML('beforeend', html) //my html is now shown
 
         }
-    } catch (err) {
+    } catch (err) { //if that file isn't there, this shall show.
         output.insertAdjacentHTML('beforeend', 'Error: ' + err)
     }
 
@@ -52,21 +52,19 @@ async function loadSchedule(file) {
 
 }
 
-
+//all 3 of these tell to load person's schedule based on what key you press on keyboard
 document.addEventListener("keydown", e => {
 if (e.key === '1') {
         loadSchedule("kristen.json")
         return;
     }
 }) 
-
 document.addEventListener("keydown", e => {
 if (e.key === '2') {
         loadSchedule("yasmina.json")
         return;
     }
 }) 
-
 document.addEventListener("keydown", e => {
 if (e.key === '3') {
         loadSchedule("amelia.json")
@@ -76,9 +74,10 @@ if (e.key === '3') {
 
 
 
-
+//resets so user can review default schedule with the click of that button
 document.getElementById("reset").addEventListener("click", () => {
     loadSchedule("shagan.json")
 })
 
+//by default, load this schedule
 loadSchedule("shagan.json")
